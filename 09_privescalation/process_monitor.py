0da27ed0b9709b3f7df93e8c42f1cf936ebf08c6
Lucas Melin <lucas.melin@gmail.com>
2021-12-22 19:02:09 -0500

    basic proccess logging

---
+++ 09_privescalation/process_monitor.py
@@ -0,0 +1,46 @@
+# Requires wmi which is a wrapper for the Windows Management Instrumentation API
+# pip install wmi
+
+import os
+import sys
+
+import win32api
+import win32con
+import win32security
+
+import wmi
+
+
+def log_to_file(message):
+    with open("process_monitor_log.csv", "a") as fd:
+        fd.write(f"{message}\r\n")
+
+
+def monitor():
+    log_to_file("CommandLine, Time, Executable, Parent PID, PID, User, Privileges")
+    c = wmi.WMI()
+    process_watcher = c.Win32_Process.watch_for("creation")
+    while True:
+        try:
+            new_process = process_watcher()
+            cmdline = new_process.CommandLine
+            create_date = new_process.CreationDate
+            executable = new_process.ExecutablePath
+            parent_pid = new_process.ParentProcessId
+            pid = new_process.ProcessId
+            proc_owner = new_process.GetOwner()
+
+            privileges = "N/A"
+            process_log_message = (
+                f"{cmdline} , {create_date} , {executable},"
+                f"{parent_pid} , {pid} , {proc_owner} , {privileges}"
+            )
+            print(process_log_message)
+            print()
+            log_to_file(process_log_message)
+        except Exception:
+            pass
+
+
+if __name__ == "__main__":
+    monitor()
